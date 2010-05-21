fcbcd89c33b1a6889a3e5a2c742bb7451e63d9c1
Shaun Ashby <Shaun.Ashby@unige.ch>
2010-05-21 16:33:32 +0200

    Update submit script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -11,12 +11,48 @@
 #
 #--------------------------------------------------------------------
 
+function write_ldif() {
+    instrument=$1
+    cn=$2
+    status=$3
+    dn="cn=${cn},ou=${instrument},ou=Datasets,dc=ashby,dc=isdc,dc=unige,dc=ch"
+     ( echo "dn: $dn"
+       echo "changetype: modify"
+       echo "replace: dsStatus"
+       echo "dsStatus: $status"
+       echo "" ) >> $LDIF
+}
+
+GPXLOGDIR=/home/ashby/gpx/logs
+JOBSCRIPT=/home/ashby/ISDC-scripts/scripts/genpixels.job.sh
+
+instrument=$1
+status=800 # For submitting
+
+if [[ -z $instrument ]]; then
+    echo "No instrument given."
+    exit 1
+fi
+
 ldapsearch -LLLL -H 'ldap://ashby.isdc.unige.ch' -x \
--b "ou=ISGRI,ou=Datasets,dc=ashby,dc=isdc,dc=unige,dc=ch" \
-'(&(dsStatus=999))' cn | grep "^cn" | sed -e 's/cn: //g' | while read cn; do
-ldapsearch -LLLL -H 'ldap://ashby.isdc.unige.ch' -x \
--b "ou=ISGRI,ou=Datasets,dc=ashby,dc=isdc,dc=unige,dc=ch" \
-"(&(dsStatus=999)(cn=${cn}))" dsHost | grep dsHost | sed -e 's/dsHost: //g' | while read host; do
-    echo "$cn $host"
-done
+    -b "ou=${instrument},ou=Datasets,dc=ashby,dc=isdc,dc=unige,dc=ch" \
+    '(&(dsStatus=999))' cn | grep "^cn" | sed -e 's/cn: //g' | while read cn; do
+    ldapsearch -LLLL -H 'ldap://ashby.isdc.unige.ch' -x \
+	-b "ou=${instrument},ou=Datasets,dc=ashby,dc=isdc,dc=unige,dc=ch" \
+	"(&(dsStatus=999)(cn=${cn}))" dsHost | grep dsHost | sed -e 's/dsHost: //g' | while read host; do
+	
+	LDIF=modify-operation-$$.ldif; [[ ! -f $LDIF ]] && touch $LDIF
+	JOBSC=job-$$.sh; [[ ! -f $JOBSC ]] && touch $JOBSC
+	
+	GPXLOGDIR=/home/ashby/gpx/logs
+	jobname=r${cn}-${instrument}-gpx
+	
+	echo qsub -N $jobname \
+	    -o ${GPXLOGDIR}/${host}/out \
+	    -e ${GPXLOGDIR}/${host}/err \
+	    -q all.q@${host} $JOBSCRIPT $instrument $cn >> $JOBSC
+	
+	write_ldif $instrument $cn $status
+	
+    done
 done
